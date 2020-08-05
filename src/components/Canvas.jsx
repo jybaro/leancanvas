675ee6c942b5d51926944cd53cd016870dcaa794
Ed <edgar.valarezo@jybaro.com>
2020-08-04 20:10:07 -0500

    Inicialización

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Canvas = () => 
+    <h1>Super canvas</h1>
+;
+
+
+export default Canvas;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Canvas from './components/Canvas';
+
+ReactDOM.render(
+    <Canvas />,
+    document.getElementById('app'),
+);
